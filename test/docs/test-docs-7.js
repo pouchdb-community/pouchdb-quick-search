7da25a80cd83b78b123b7864985f43848a01bab4
Nolan Lawson <nolan.lawson@gmail.com>
2014-06-12 09:25:01 -0700

    start testing other languages

---
+++ test/docs/test-docs-7.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = [
+  {
+    _id : '1',
+    text : 'This is text in English about the spleen, which is an organ.'
+  },
+  {
+    _id: '2',
+    text: 'Ça c\'est du texte français qui mentionne le spleen, ce qui veut dire ennui.'
+  }
+];
